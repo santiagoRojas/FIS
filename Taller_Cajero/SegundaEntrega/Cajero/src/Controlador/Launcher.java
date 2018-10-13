e0560c5e0c8b596285887a5c1050602d8eada6eb
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2018-10-13 17:13:34 -0500

    Add files via upload

---
+++ Taller_Cajero/SegundaEntrega/Cajero/src/Controlador/Launcher.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Vista.Bienvenida;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Bienvenida comienzo = new Bienvenida();
+        comienzo.setVisible(true);
+    }
+}
